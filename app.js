f5f963f79d6669ee1b76c4db7a1a238f3463559b
DeclanNYC <hf970@nyu.edu>
2020-02-05 12:27:21 +0000

    add authenticate to sharing process

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ var Comment = require("./models/comment");
 var User = require("./models/user");
 var seedDB = require("./seeds");
 
+var commentRoutes    = require("./routes/comments"),
+	campgroundRoutes = require("./routes/campgrounds"),
+	authRoutes       = require("./routes/auth");
+
 mongoose.connect("mongodb://localhost/sharing_bay", {
 	useNewUrlParser: true,
 	useUnifiedTopology: true
@@ -18,7 +22,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
-seedDB();
+// seedDB();
 
 // PASSPORT CONFIGURATION
 app.use(require("express-session")({
@@ -37,122 +41,13 @@ app.use(function(req, res, next){
 	next();
 });
 
+app.use(authRoutes);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/comments", commentRoutes);
+
 app.get("/", function(req, res){
     res.render("landing");
 });
-
-app.get("/campgrounds", function(req, res){
-	Campground.find({}, function(err, allCampgrounds) {
-		if (err) {
-			console.log(err);
-		}
-		else {
-			res.render("campgrounds/campgrounds", {campgrounds: allCampgrounds});
-		}
-	})
-});
-
-app.get("/campgrounds/new", function(req, res){
-    res.render("campgrounds/new");
-});
-
-app.post("/campgrounds", function(req, res){
-    var name = req.body.name;
-    var image = req.body.image;
-	var description = req.body.description;
-    var newCampground = {
-        name: name,
-        image: image,
-		description: description
-    };
-	Campground.create(newCampground, function(err, newlyCreated) {
-		if (err) {
-			console.log(err);
-		}
-		else {
-			res.redirect("/campgrounds");
-		}
-	});
-});
-
-app.get("/campgrounds/:id", function(req, res) {
-	Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground) {
-		if (err) {
-			console.log(err);
-		}
-		else {
-			res.render("campgrounds/show", {campground: foundCampground});
-		}
-	});
-});
-
-app.get("/campgrounds/:id/comments/new", isLoggedIn, function(req, res){
-	Campground.findById(req.params.id, function(err, campground){
-		if(err) {
-			console.log(err);
-		}
-		else {
-			res.render("comments/new", {campground: campground});
-		}
-	});
-});
-
-app.post("/campgrounds/:id/comments", isLoggedIn, function(req, res){
-	Campground.findById(req.params.id, function(err, campground){
-		if(err) {
-			console.log(err);
-			res.redirect("/campgrounds");
-		}
-		else {
-			Comment.create(req.body.comment, function(err, comment){
-				if(err) {
-					console.log(err);
-				}
-				else {
-					campground.comments.push(comment);
-					campground.save();
-					res.redirect("/campgrounds/" + campground._id);
-				}
-			});
-		}
-	});
-});
-
-app.get("/register", function(req, res){
-	res.render("register");
-});
-
-app.post("/register", function(req, res){
-	var newUser = new User({username: req.body.username});
-	User.register(newUser, req.body.password, function(err, user){
-		if(err) {
-			console.log(err);
-			return res.render("register");
-		}
-		passport.authenticate("local")(req, res, function(){
-			res.redirect("/campgrounds");
-		});
-	});
-});
-
-app.get("/login", function(req, res){
-	res.render("login");
-});
-
-app.post("/login", passport.authenticate("local", {successRedirect: "/", failureRedirect: "/login"}), function(req, res){
-});
-
-app.get("/logout", function(req, res){
-	req.logout();
-	res.redirect("/");
-});
-
-function isLoggedIn(req, res, next){
-	if(req.isAuthenticated()){
-		return next();
-	}
-	res.redirect("/login");
-}
 
 app.listen(3000, process.env.IP, function(){
     console.log("The server is listening on port 3000 or GoormIDE's port, so please go to check http://localhost:3000 or contact the admin");
