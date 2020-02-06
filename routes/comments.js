63bcd121fcb7095a94cce136de38f249dd590f31
DeclanNYC <hf970@nyu.edu>
2020-02-06 04:04:42 +0000

    add middleware sothat people can delete comments

--- routes/comments.js
+++ routes/comments.js
@@ -38,6 +38,38 @@ router.post("/", isLoggedIn, function(req, res){
 	});
 });
 
+router.get("/:comment_id/edit", checkCommentOwnership, function(req, res){
+	Comment.findById(req.params.comment_id, function(err, findComment){
+		if(err) {
+			res.redirect("back");
+		}
+		else {
+			res.render("comments/edit", {campground_id: req.params.id, comment: findComment});
+		}
+	});
+});
+
+router.put("/:comment_id", checkCommentOwnership, function(req, res){
+	Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment){
+		if(err) {
+			res.redirect("back");
+		}
+		else {
+			res.redirect("/campgrounds/" + req.params.id);
+		}
+	});
+});
+
+router.delete("/:comment_id", checkCommentOwnership, function(req, res){
+	Comment.findByIdAndRemove(req.params.comment_id, function(err){
+		if(err) {
+			res.redirect("back");
+		}
+		else {
+			res.redirect("/campgrounds/" + req.params.id);
+		}
+	});
+});
 
 // middleware
 function isLoggedIn(req, res, next){
@@ -45,6 +77,27 @@ function isLoggedIn(req, res, next){
 		return next();
 	}
 	res.redirect("/login");
+}
+
+function checkCommentOwnership(req, res, next) {
+	if(req.isAuthenticated()) {
+		Comment.findById(req.params.comment_id, function(err, foundComment){
+			if(err) {
+				res.redirect("back");
+			}
+			else {
+				if(foundComment.author.id.equals(req.user._id)) {
+					next();
+				}
+				else {
+					res.redirect("back");
+				}
+			}
+		});
+	}
+	else {
+		res.redirect("back");
+	}
 }
 
 module.exports = router;
